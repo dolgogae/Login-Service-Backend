adaa70435fd34bfa74e421729e0b41490a30610d
sihun <34716571+dolgogae@users.noreply.github.com>
2023-09-07 00:08:46 +0900

    feat: user create, update 컬럼추가

--- src/main/java/local/sihun/springsec/SpringSecApplication.java
+++ src/main/java/local/sihun/springsec/SpringSecApplication.java
@@ -6,10 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.persistence.EntityListeners;
+
 @EnableCaching
+@EnableJpaAuditing
 @SpringBootApplication//(exclude={SecurityAutoConfiguration.class})
 public class SpringSecApplication {
 
--- src/main/java/local/sihun/springsec/domain/user/data/UserEntity.java
+++ src/main/java/local/sihun/springsec/domain/user/data/UserEntity.java
@@ -5,13 +5,18 @@ import local.sihun.springsec.domain.user.dto.UserDto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @Table(name = "USER")
+@EntityListeners(AuditingEntityListener.class)
 public class UserEntity {
 
     @Id
@@ -37,6 +42,12 @@ public class UserEntity {
 
     @Column(name = "REFRESH_TOKEN", length = 2000)
     private String refreshToken;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
     @Builder
     private UserEntity(String username, String email, String password, UserRole userRole) {
